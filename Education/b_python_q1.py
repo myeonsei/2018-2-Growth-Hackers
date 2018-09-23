904cf6d880a924179eba867a3bde8a785f8d0bb2
myeonsei <38792588+myeonsei@users.noreply.github.com>
2018-09-23 16:37:59 +0900

    Update b_python_q1.py

--- Education/b_python_q1.py
+++ Education/b_python_q1.py
@@ -1,3 +1,4 @@
+# 1안
 def solution(n, money):
     money.sort(reverse=True)
     answer = 0
@@ -5,6 +6,19 @@ def solution(n, money):
         if n > m:
             answer += solution(n-m, money[i:])
         elif n == m: answer += 1
+    return answer
+    
+# 2안
+def solution(n, money):
+    money.sort()
+    if n < money[0]: return 0
+    answer = 0
+    for i, m in enumerate(money):
+        if n > m: 
+            answer += solution(n-m, money[i:])
+        elif n == m:
+            answer +=1
+            break
     return answer
     
 # 효율성 문제 해결이 안됨. ^_^;;
